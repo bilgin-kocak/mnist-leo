5d1f7211672c095b037ba3bb6f075a5780b70a08
bilgin-kocak <kocakbilgin@gmail.com>
2023-11-17 11:20:46 +0300

    feat: aleo neural network added

---
+++ backend/helloworld/deploy.sh
@@ -0,0 +1,5 @@
+
+
+snarkos developer deploy nn_mnist.aleo --private-key APrivateKey1zkpDFP8iVqYamDACpRmqg8hzp35hHiK7yrBJLRYeT2cmoSq --priority-fee 1000000 --path "./build/" --query "https://vm.aleo.org/api" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast"
+
+
